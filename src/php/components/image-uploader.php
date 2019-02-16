d321043db1cd29320eb8248125843d21b8199a66
Jordan Pinski <jpinski@tritoncommerce.com>
2019-02-16 12:36:36 -0600

    Added low resolution images. Need to resize images & create the zip download

--- src/php/components/image-uploader.php
+++ src/php/components/image-uploader.php
@@ -9,6 +9,9 @@ use \Gumlet\ImageResize;
 class ImageUploader {
 
   private $max_file_size = 8000000; // 1Mb = 8e+6bytes
+  private $output_directory = 'images/';
+  private $image_size_small = 768;
+  private $image_size_medium = 992;
 
   function __construct() {
 
@@ -25,47 +28,135 @@ class ImageUploader {
 
       $uploaded_files = $_FILES['file_upload'];
 
-
       foreach ($uploaded_files['tmp_name'] as $index=>$file_tmp_name) {
-        $this->resizeImage($file_tmp_name, $uploaded_files['name'][$index], 'jpg');
+        $this->resizeImage($file_tmp_name, $uploaded_files['name'][$index]);
+
+
       }
+
+      // $this->zipImages();
+      // $this->cleanOutputDirectory();
+
     }
+  }
+
+  /**
+   * Zips all images in $this->$output_directory
+   */
+  function zipImages() {
+    $zip = new ZipArchive();
+    $compress = $zip->open("images/images.zip", ZIPARCHIVE::CREATE);
+
+    if ($compress === true) {
+      $zip->addFile($image);
+    }
+
+    $zip->close();
+  }
+
+  /**
+   * Cleans the output directory by deleting all of the files inside.
+   */
+  function cleanOutputDirectory() {
+
   }
 
   /**
    * Resizes the given image based on its size.
    *
+   * lr, sm, md, lg
+   * 
+   * RESIZE DIMENSIONS
+   * sm: 768px;
+   * md: 992px;
+   * 
    * < 768px - LowRes, Small (SAME SIZE)
    * >= 768px AND <= 1200px - LowRes, Small, Medium (SAME SIZE)
-   * > 1200px - LowRes, Small, Medium, Large(SAME SIZE) 
-   * 
-   * TODO: 
+   * > 1200px - LowRes, Small, Medium, Large(SAME SIZE)
    */
-  function resizeImage($file_tmp_name, $name, $type) {
-    $image = new ImageResize($file_tmp_name);
+  function resizeImage($file_tmp_name, $name) {
 
     $name = explode('.', $name);
     $image_name = $name[0];
     $image_type = $name[1];
+    $image_width = getimagesize($file_tmp_name)[0];
 
     // Small Image
-    if ($image->getSourceWidth() < 768) {
+    if ($image_width < 768) {
 
       // Low Resolution
-      $image->save("images/$image_name-small-lr.$image_type", null, 100);
+      $this->saveImage($file_tmp_name, $image_name . '-lr', $image_type, 10);
 
-      // Same Size
-      $image->save("images/$image_name-small.$image_type", null, 100);
+      // Same size
+      $this->saveImage($file_tmp_name, $image_name . '-sm', $image_type, 100);
     }
 
     // Medium Image
-    if ($image->getSourceWidth() >= 768 && $image->getSourceWidth() < 1200) {
-      $image->save("images/$image_name-medium.$image_type");
+    if ($image_width >= 768 && $image_width < 1200) {
+    
+      // Low Resolution
+      $this->saveImage($file_tmp_name, $image_name . '-lr', $image_type, 10);
+
+      // Small
+      $this->saveImage($file_tmp_name, $image_name . '-sm', $image_type, 100, 'sm');
+
+      // Same size
+      $this->saveImage($file_tmp_name, $image_name . '-md', $image_type, 100);
     }
 
     // large Image
-    if ($image->getSourceWidth() > 1200) {
-      $image->save("images/$image_name-large.$image_type");
+    if ($image_width > 1200) {
+
+      // Low Resolution
+      $this->saveImage($file_tmp_name, $image_name . '-lr', $image_type, 10);
+
+      // Small
+      $this->saveImage($file_tmp_name, $image_name . '-sm', $image_type, 100, 'sm');
+
+      // Medium
+      $this->saveImage($file_tmp_name, $image_name . '-md', $image_type, 100, 'md');
+
+      // Same size
+      $this->saveImage($file_tmp_name, $image_name . '-lg', $image_type, 100);
+    }
+  }
+
+  /**
+   * Saves an image to /images.
+   * 
+   * TODO: Resize the images.
+   */
+  function saveImage($file_tmp_name, $image_name, $image_type, $quality, $size = null) {
+
+    $destination = $this->output_directory . "/$image_name.$image_type";
+
+    if ($image_type == 'png') {
+
+      $image = imagecreatefrompng($file_tmp_name);
+
+      // PNG quality must be 0 (Highest) to 9 (Lowest)
+      // Turn scale 0 - 100 into 0 - 9.
+      $quality =  10 - ( ceil($quality) / 10 );
+
+      // Resize the image
+      if ($size) {
+
+      }
+
+      imagepng($image, $destination, $quality);
+      imagedestroy($image);
+      
+    } else if ($image_type == 'jpg') {
+
+      $image = @imagecreatefromjpeg($file_tmp_name);
+
+      // Resize the image
+      if ($size) {
+
+      }
+
+      imagejpeg($image, $destination, $quality);
+      imagedestroy($image);
     }
   }
 
